99b111cd86e26000d1e6de8731a45c879604c0c7
rafaelmadaloz <rafael.madaloz@grad.ufsc.br>
2018-08-12 21:44:55 -0300

    commit2

---
+++ src/main/java/controle/Produto.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle;
+
+public class Produto {
+    
+    int codigo;
+    float preco;
+    String nome;
+    String endereco_imagem;
+    String descricao;
+    
+    public Produto(int codigo, float preco, String nome, String endereco_imagem, String descricao) {
+        this.codigo = codigo;
+        this.preco = preco;
+        this.nome = nome;
+        this.endereco_imagem = endereco_imagem;
+        this.descricao = descricao;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco_imagem() {
+        return endereco_imagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereco_imagem(String endereco_imagem) {
+        this.endereco_imagem = endereco_imagem;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+}
