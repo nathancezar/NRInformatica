fcd975d430997a7f7fabd6af12287eb3c65b24f2
rafaelmadaloz <rafael.madaloz@grad.ufsc.br>
2018-08-24 17:38:23 -0300

    add ver produtos da lista

--- src/main/java/controle/Main.java
+++ src/main/java/controle/Main.java
@@ -17,9 +17,24 @@ public class Main {
         Produto produtoP1 = new Produto(1000, 30, 49.90f, "Mouse Optico",
                 "endereco", "Mouse Optico Razer 1800DPI Modelo 075 Nagius");
         
-        //cadastro.novoProduto();
-
+        Produto produtoP2 = new Produto(1001, 24, 49.90f, "Teclado sem fio",
+                "endereco", "Mouse Optico Razer 1800DPI Modelo 075 Nagius");
+        
+        Produto produtoP3 = new Produto(1002, 40, 49.90f, "Roteador Wi-fi",
+                "endereco", "Mouse Optico Razer 1800DPI Modelo 075 Nagius");
+        
+        Produto produtoP4 = new Produto(1003, 40, 49.90f, "Impresso jato de tinta",
+                "endereco", "Mouse Optico Razer 1800DPI Modelo 075 Nagius");
+        
+        Produto produtoP5 = new Produto(1004, 3, 49.90f, "Pendrive Sandisk 8GB",
+                "endereco", "Mouse Optico Razer 1800DPI Modelo 075 Nagius");
+        
+ 
         cadastro.adicionarProduto(produtoP1);
+        cadastro.adicionarProduto(produtoP2);
+        cadastro.adicionarProduto(produtoP3);
+        cadastro.adicionarProduto(produtoP4);
+        cadastro.adicionarProduto(produtoP5);
         
         cadastro.verProdutosCadastrados();
     }
